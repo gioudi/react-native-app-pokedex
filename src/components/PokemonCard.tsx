f3e2ae146b4d0ba7c587ebe0cf09fdcf084255d0
Semi-dios <tipouno@misena.edu.co>
2022-09-03 19:46:05 -0500

    feat-component-card-image/build_card_add_get_color_images

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,124 @@
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
+import {PayloadPokemon} from '../interfaces/pokemonInterfaces';
+import {FadeInImage} from './FadeInImage';
+import ImageColors from 'react-native-image-colors';
+
+const windowWidth = Dimensions.get('window').width;
+
+interface Props {
+  pokemon: PayloadPokemon;
+}
+
+export const PokemonCard = ({pokemon}: Props) => {
+  const [BgColor, setBgColor] = useState('#B6B6B6');
+
+  useEffect(() => {
+    getColorsPokemons();
+  }, []);
+
+  const getColorsPokemons = async () => {
+    const getColorPokemon = await ImageColors.getColors(pokemon.picture, {
+      fallback: '#228B22',
+    });
+    switch (getColorPokemon.platform) {
+      case 'android':
+        setBgColor(getColorPokemon.dominant);
+        break;
+      case 'web':
+        setBgColor(getColorPokemon.lightVibrant);
+        break;
+      case 'ios':
+        setBgColor(getColorPokemon.background);
+        break;
+      default:
+        throw new Error('Unexpected platform key');
+    }
+  };
+
+  return (
+    <TouchableOpacity activeOpacity={0.7}>
+      <View
+        style={{
+          ...styles.pkdCardContainer,
+          width: windowWidth * 0.4,
+          backgroundColor: BgColor,
+        }}>
+        <View>
+          <Text style={styles.pkdCardContainer__Name}>
+            {pokemon.name}
+            {'\n#' + pokemon.id}
+          </Text>
+        </View>
+
+        <View style={styles.pkdCardContainer__PokebolaContainer}>
+          <Image
+            source={require('../assets/assets/pokebola-blanca.png')}
+            style={styles.pkdCardContainer__Pokebola}
+          />
+        </View>
+
+        <FadeInImage
+          uri={pokemon.picture}
+          style={styles.pkdCardContainer__Image}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  pkdCardContainer: {
+    marginHorizontal: 10,
+
+    height: 120,
+    width: 160,
+    marginBottom: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  },
+  pkdCardContainer__Name: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    top: 20,
+    left: 10,
+  },
+  pkdCardContainer__Pokebola: {
+    width: 100,
+    height: 100,
+    opacity: 0.7,
+    position: 'absolute',
+    bottom: -20,
+    right: -20,
+  },
+  pkdCardContainer__PokebolaContainer: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    overflow: 'hidden',
+  },
+  pkdCardContainer__Image: {
+    width: 120,
+    height: 120,
+    position: 'absolute',
+    right: -8,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {ActivityIndicator, FlatList, Image, Text, View} from 'react-native';
-import {color} from 'react-native-reanimated';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {FadeInImage} from '../components/FadeInImage';
+import {PokemonCard} from '../components/PokemonCard';
 import {usePokemonPaginate} from '../hooks/usePokemonPaginate';
 import {styles} from '../theme/styles';
 export const HomeScreen = () => {
@@ -16,20 +15,36 @@ export const HomeScreen = () => {
         style={styles.pokebolaBG}
       />
 
-      <FlatList
-        data={payloadPokemon}
-        keyExtractor={pokemon => pokemon.id}
-        showsVerticalScrollIndicator={false}
-        renderItem={({item}) => (
-          <FadeInImage uri={item.picture} style={{width: 100, height: 100}} />
-        )}
-        //Infinete Scroll
-        onEndReached={loadPokemons}
-        onEndReachedThreshold={0.4}
-        ListFooterComponent={
-          <ActivityIndicator style={{height: 100}} size={20} color="grey" />
-        }
-      />
+      <View
+        style={{
+          alignItems: 'center',
+        }}>
+        <FlatList
+          data={payloadPokemon}
+          keyExtractor={pokemon => pokemon.id}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          //Header
+          ListHeaderComponent={
+            <Text
+              style={{
+                ...styles.pkdTitle,
+                ...styles.pkdContainer,
+                top: top + 20,
+                marginBottom: top + 80,
+              }}>
+              Pokedex
+            </Text>
+          }
+          renderItem={({item}) => <PokemonCard pokemon={item} />}
+          //Infinete Scroll
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          ListFooterComponent={
+            <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+          }
+        />
+      </View>
     </>
   );
 };
